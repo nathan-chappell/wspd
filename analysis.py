7c6d29419d163cd125ec779539f9d4226034a71d
nathan-chappell <nathan.s.chappell@gmail.com>
2019-09-17 15:02:09 +0200

    ready

--- analysis.py
+++ analysis.py
@@ -17,19 +17,49 @@ def print_basic_info(result):
 def get_np_for_dumbells(dumbell_info):
   return np.array([[db['s'],db['lr'],db['rr']] for db in dumbell_info])
 
+target_sep_grid_map = {
+  .25:0,
+  .5:1,
+  1:2,
+  1.5:3
+}
+
+n_components_grid_map = {
+  5:1,
+  10:2,
+  20:3
+}
+
+columns = len(target_sep_grid_map)
+rows = len(n_components_grid_map)
+def get_subplt_index(target_sep, n_components):
+  return target_sep_grid_map[target_sep]*rows + n_components_grid_map[n_components]
+
 for result in results:
   print_basic_info(result)
+  target_sep = result['target_sep']
+  n_components = int(result['n_components'])
   low_d = get_np_for_dumbells(result['low_d_info'])
   hi_d = get_np_for_dumbells(result['hi_d_info'])
   num_dumbells = len(low_d)
-  plt.scatter(np.arange(num_dumbells), low_d[:,0],c='b')
-  plt.scatter(np.arange(num_dumbells), hi_d[:,0],c='r')
+  #plt.subplot(rows,columns,get_subplt_index(target_sep,n_components))
+  plt.scatter(np.arange(num_dumbells), low_d[:,0],c=(0,0,1,.8))
+  plt.scatter(np.arange(num_dumbells), hi_d[:,0],c=(1,0,0,.8))
+  plt.plot(np.arange(num_dumbells),np.ones(num_dumbells)*target_sep,c='k')
+  plt.plot(np.arange(num_dumbells),np.ones(num_dumbells)*0,c='k')
   plt.yscale('symlog',basey=2)
+  plt.title('shape:' + str(result['shape']) + ', dumbells: ' +
+                str(len(result['dumbell_indices'])) + '\n' +
+                'target_sep: ' + str(target_sep) + ', n_components: ' + 
+                str(n_components))
   #plt.savefig('arpack_sep_' + str(result['target_sep']) + '.png')
   plt.savefig('sep_' + 
-               str(result['target_sep']) + '.' +
-               str(int(result['n_components'])) + 
+               str(target_sep) + '.' +
+               str(n_components) + 
                '.png')
+  plt.clf()
   #plt.show()
+
+plt.savefig('combined.png')
 
 
